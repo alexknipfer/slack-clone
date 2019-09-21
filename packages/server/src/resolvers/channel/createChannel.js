aab8a0e18f3748b5387a33c33a1dc67a030eb07d
alexknipfer <alexanderknipfer@gmail.com>
2019-09-21 18:11:19 -0400

    Remove

--- packages/server/src/resolvers/channel/createChannel.js
+++
@@ -1,8 +0,0 @@
-import { Channel } from '../../model/channel';
-
-export const createChannel = (obj, args) => {
-  return Channel.create({
-    name: args.name,
-    public: args.public
-  });
-};
--- packages/server/src/typeDefs/index.js
+++
@@ -1,6 +0,0 @@
-import { root } from './root';
-import { user } from './user';
-import { message } from './message';
-import { channel } from './channel';
-
-export const typeDefsSchema = [root, user, message, channel];
