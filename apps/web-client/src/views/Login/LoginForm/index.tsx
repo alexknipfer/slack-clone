a74fcce9533d259f7e0bba107d6d84d8d48ff3b4
alexknipfer <alexanderknipfer@gmail.com>
2019-09-22 00:29:09 -0400

    Use formik hook

--- apps/web-client/src/views/Login/LoginForm/index.tsx
+++ apps/web-client/src/views/Login/LoginForm/index.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import * as Yup from 'yup';
-import { Formik, Form } from 'formik';
+import { Form, useFormik } from 'formik';
 
 import { EMAIL_REGEX } from '../../../util/regex';
 import { Input } from '../../../components/Input';
 import { Space } from '../../../style';
 import { Button } from '../../../components/Button';
+import { AuthContext } from 'apps/web-client/src/stores/auth/context';
 
 const SignInSchema = Yup.object().shape({
   email: Yup.string()
@@ -14,32 +15,34 @@ const SignInSchema = Yup.object().shape({
   password: Yup.string().required('Password is required.')
 });
 
-export const LoginForm: React.FC = () => (
-  <Formik
-    initialValues={{ email: '', password: '' }}
-    validationSchema={SignInSchema}
-    onSubmit={values => console.log(values)}
-    render={({ values, handleChange, errors }) => (
-      <Form>
-        <Input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={values.email}
-          onChange={handleChange}
-          errorMessage={errors.email}
-        />
-        <Space height={10} />
-        <Input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={values.password}
-          onChange={handleChange}
-        />
-        <Space height={15} />
-        <Button type="submit" text="Sign In" size="large" />
-      </Form>
-    )}
-  ></Formik>
-);
+export const LoginForm: React.FC = () => {
+  const { login } = useContext(AuthContext);
+  const { values, errors, handleChange, handleSubmit } = useFormik({
+    initialValues: { email: '', password: '' },
+    onSubmit: values => login(values.email, values.password),
+    validationSchema: SignInSchema
+  });
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={values.email}
+        onChange={handleChange}
+        errorMessage={errors.email}
+      />
+      <Space height={10} />
+      <Input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={values.password}
+        onChange={handleChange}
+      />
+      <Space height={15} />
+      <Button type="submit" text="Sign In" size="large" />
+    </form>
+  );
+};
